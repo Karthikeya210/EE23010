d56ac864f8ebaf62cfe711e67ccd5ebe8d39e2a1
Karthikeya210 <139639550+Karthikeya210@users.noreply.github.com>
2023-09-17 14:47:44 +0530

    Update main.py

--- 70/codes/main.py
+++ 70/codes/main.py
@@ -1,31 +1,18 @@
 import numpy as np
-from scipy.optimize import fsolve
+#let X and Y be the two bernoulli random variables where 
 
-# Define the values of X and their corresponding probabilities
-# You should define A before using it in X_values
-# You can change this value to your desired initial guess
+pX = 1/2 #basically its x it can be changed 
+pY = 1/6 #basically its y it can be changed 
 
-X_values = np.array([1, 2, 4, 0,0,0])
-P_X = np.array([1/2, 1/5, 3/25, 1/10, 1/25, 1/25])
+pXY = pX * pY #Given that they are independent
+pXunionY = pX + pY 
 
-# Define a function that represents E(X) 
-def equation_to_solve(A):
-    X_values[3] = 2 * A
-    X_values[4] = 3 * A
-    X_values[5] = 5 * A
-    # Calculate E(X)
-    E_X = np.sum(X_values * P_X)
-    return E_X - 2.94
+if (pXY==0):
+	print("X and Y are mutually exclusive")
+else:
+	print("X and Y are not mutually exclusive")
+if (pXunionY!=1):
+	print("It is not necessary that probability of X + Y is 1")
 
 
-# Use fsolve to find the value of A that satisfies the equation E(X) - 2.94 = 0
-A = fsolve(equation_to_solve, 1.0)[0]
 
-# Calculate E(X^2) using the correct value of A
-E_X_squared = np.sum((X_values ** 2) * P_X)
-
-# Calculate the variance Var(X)
-Var_X = E_X_squared - (2.94 ** 2)
-
-print("Value of A:", A)
-print("Variance of X:", Var_X)
